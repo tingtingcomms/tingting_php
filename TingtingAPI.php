a197528ce0b4633bbea141da4e1a658f0068c362
duongdx <duong.d@scopicsoftware.com>
2021-10-27 10:23:29 +0700

    Add zns api

--- TingtingAPI.php
+++ TingtingAPI.php
@@ -10,6 +10,60 @@ class TingtingAPI {
         $this->apiKey = $key;
     }
 
+    public function sendZNS($to, $sender, $tempid, $tempdata, $failoverdata = null, $sendTime = '', $timezone = '') {
+
+        $params = [
+            'to' => $to,
+            'sender' => $sender,
+            'tempid' => $tempid,
+            'temp_data' => $tempdata
+        ];
+
+        if (!empty($failoverdata) && is_array($failoverdata)) {
+
+            if (!isset($failoverdata['sender']) || !isset($failoverdata['content'])) {
+                return null;
+            }
+
+            $params['failover'] = $failoverdata;
+        }
+
+        if (!empty($sendTime)) {
+            $params['send_time'] = $sendTime;
+        }
+        if (!empty($timezone)) {
+            $params['timezone'] = $timezone;
+        }
+
+        $json = json_encode($params);
+
+        $headers = array('Content-type: application/json', 'apikey: '.$this->apiKey);
+
+        $url = $this->API_URL.'/zns';
+        $http = curl_init($url);
+        curl_setopt($http, CURLOPT_HEADER, false);
+        curl_setopt($http, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($http, CURLOPT_POSTFIELDS, $json);
+        curl_setopt($http, CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($http, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($http, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($http, CURLOPT_VERBOSE, 0);
+        curl_setopt($http, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        curl_setopt($http, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($http, CURLOPT_HTTPHEADER, $headers);
+        $result = curl_exec($http);
+
+        if(curl_errno($http))
+        {
+            return null;
+        }
+        else
+        {
+            curl_close($http);
+            return json_decode($result, true);
+        }
+    }
+
     public function sendSMS($to, $sender, $content, $sendTime = '', $timezone = '') {
         $params = [
             'to' => $to,
